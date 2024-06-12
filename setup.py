0fe5c3ad5c0c931291b495b09db175e31cafc705
Federico Iza <fedegiza@gmail.com>
2024-06-12 14:44:49 -0300

    Fix instalation files.

--- setup.py
+++ setup.py
@@ -2,4 +2,7 @@ from setuptools import find_packages, setup
 
 setup(name="nbody",
       packages=find_packages(),
+      version="0.0.1",
+      author="Federico Iza",
+      author_email="fedegiza@gmail.com",
       )
